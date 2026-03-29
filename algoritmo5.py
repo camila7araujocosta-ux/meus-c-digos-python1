c7e0940e9c193c7f84eee9322de3fbdd218da5d1
Camila <camila7araujocosta@gmail.com>
2026-03-29 17:35:52 -0300

    Adicionei algoritmo5.py

---
+++ algoritmo5.py
@@ -0,0 +1,9 @@
+#Algoritmo que receba a altura e o peso de uma pessoa e calcule o IMC
+#Lê a altura e converte para float
+altura = float(input("Informe a altura: "))
+#Lê o peso em Kg e converte para float
+peso = float(input("Informe o peso em Kg: "))
+#Calcule o IMC
+IMC = peso / altura **2
+#Mostre o resultado do IMC
+print("O resultdado do IMC é: ", IMC)
