e6bc07ad636fb133a58c95015a84f3f4ba903d09
Camila <camila7araujocosta@gmail.com>
2026-03-29 16:51:37 -0300

    Adicionei algoritmo2.py

--- algoritmo.py
+++ algoritmo.py
@@ -1,12 +1,11 @@
 # Algoritmo que recebe dois números e os multiplica
 # Lê o primeiro número e converte para inteiro
 N1 = int(input("Informe um número: "))
-
 # Lê o segundo número e converte para inteiro
 N2 = int(input("Informe outro número: "))
-
 # Faz a multiplicação 
 R = N1 * N2
-
 # Mostra o resultado 
 print("O resultado da multiplicação é: ",R)
+#mostre resultado da multiplicação 
+print("O resultado da multiplicação é: ", R)
---
+++ algoritmo2.py
@@ -0,0 +1,15 @@
+#Algoritmo que receba cinco números e calcule a média aritmética 
+# Lê o primeiro resultado 
+N1 = float(input("Informe um número: "))
+#Lê o segundo número         
+N2 = float(input("Informe outro número: ")) 
+#Lê o terceiro número
+N3 = float(input("Informe outro número: "))
+#Lê o quarto número
+N4 = float(input("Informe outro número: "))
+#Lê o quinto número
+N5 = float(input("Informe outro número: "))
+#calcula a média aritmética
+M = N1 +N2 + N3 + N4 + N5 / 5
+#Mostre resultado da média aritmética
+print("O resultado da média é: ", M)
